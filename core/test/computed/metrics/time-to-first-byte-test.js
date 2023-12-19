2d43de01f094fb655f5c5cc5c5d69bc523cf93fc
Connor Clark <cjamcl@google.com>
2023-12-19 14:20:37 -0800

    core(network-request): consider secondary headers for content encoded check (#15708)

--- core/test/computed/metrics/time-to-first-byte-test.js
+++ core/test/computed/metrics/time-to-first-byte-test.js
@@ -44,7 +44,7 @@ function mockNetworkRecords() {
     transferSize: 300,
     url: requestedUrl,
     frameId: 'ROOT_FRAME',
-    responseHeaders: [{name: 'Content-Encoding'}],
+    responseHeaders: [{name: 'Content-Encoding', value: 'gzip'}],
   },
   {
     requestId: '2:redirect',
@@ -58,7 +58,7 @@ function mockNetworkRecords() {
     transferSize: 16_000,
     url: mainDocumentUrl,
     frameId: 'ROOT_FRAME',
-    responseHeaders: [{name: 'Content-Encoding'}],
+    responseHeaders: [{name: 'Content-Encoding', value: 'gzip'}],
   }];
 }
 
